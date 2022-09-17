e1336eada2fc8294696613620bec0755a88e1508
ZulfaFebiAfidria <113028263+ZulfaFebiAfidria@users.noreply.github.com>
2022-09-17 10:27:04 -0400

    Add files via upload

---
+++ output2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Inisialisasi Var
+a=55;
+b=4;
+distroLinux="Ubuntu 9.04 LTS";
+let c=a%b;
+
+# Output Printf
+printf " OS : $distroLinux \n";
+prinft "$c \n";
+printf "%.2f float \n" $a;
+printf "%.1f float \n" $a;
